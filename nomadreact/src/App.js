2be7d3b61e80d8ab148d4487c65a5827a7b230fe
minkyung <alsrudsla1124@gmail.com>
2022-11-23 14:37:06 +0900

    toDoList

--- nomadreact/src/App.js
+++ nomadreact/src/App.js
@@ -1,27 +1,36 @@
 import {useState, useEffect} from 'react';
 
-function Hello () {
-
-  useEffect (() => {
-    console.log('hi');
-    return () => console.log('bye');
-  }, []);
-
-  return <h1>Hello</h1>
-}
-
 function App() {
 
-  const [showing, setShowing] = useState(false);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
 
-  const onClick = () =>{
-    setShowing(current => !current);
+  const onChange = (event) => {
+    setTodo(event.target.value);
+    // console.log(event.target.value);
   }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setTodos(current => [toDo, ...current]);
+    setTodo("");
+    console.log(toDos);
+  }
+
 
   return (
     <div className="App">
-      {showing ? <Hello /> : ""}
-        <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>To Do List ({toDos.length})</h1>
+      <form type="submit" onSubmit={onSubmit}>
+        <input value={toDo} onChange={onChange} type="text" placeholder='Write your todo' />
+        <button type='submit'>Add</button>
+      </form>
+      <hr />
+      {
+        toDos.map((item, index) =>
+          <li key={index}>{item}</li>
+        )
+      }
     </div>
   );
 }
